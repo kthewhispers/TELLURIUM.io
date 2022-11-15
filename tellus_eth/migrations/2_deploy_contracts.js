1fc471a943aefa16f96794cae6eb7f9fdd482d13
Keith Cronin <39650357+kthewhispers@users.noreply.github.com>
2022-11-14 21:18:15 -0500

    Delete tellus_eth directory

--- tellus_eth/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const RegistryEntities = artifacts.require("RegistryEntities");
-const Users = artifacts.require("Users");
-
-module.exports = function(deployer) {
-  deployer.deploy(RegistryEntities);
-  deployer.deploy(Users);
-};
